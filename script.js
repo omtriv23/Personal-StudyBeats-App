f062b07ff3179f32559b270f6f93b726e11b2b73
omtriv23 <omtriv23@gmail.com>
2026-08-05 22:12:53 -0400

    Added uploads of first files for StudyBeats app.
    Created basic structure for the HTML/CSS category. Started by adding an alert to come at button press through the JS code. Created the index.html, style.css, and script.js files. Mainly focused on styling and web page setup so far...

---
+++ script.js
@@ -0,0 +1,7 @@
+// Main Javascript code file for my study app
+const getStartedButton = document.getElementById("getStarted");
+
+getStartedButton.onclick = function welcomeAlert() {
+    console.log("Get Started button was successfully clicked!");
+    alert("Welcome to StudyBeats!");
+};

173823557c108d92bc9a57c969274a8b60b96707
omtriv23 <omtriv23@gmail.com>
2026-08-07 21:45:30 -0400

    Implement welcome modal for user interaction
    Refactored welcome alert functionality to use a modal instead of an alert. Also added open/close functions at the press of a button in the JS code.

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 // Main Javascript code file for my study app
-const getStartedButton = document.getElementById("getStarted");
 
-getStartedButton.onclick = function welcomeAlert() {
-    console.log("Get Started button was successfully clicked!");
-    alert("Welcome to StudyBeats!");
-};
+// Variables that retreive HTML elements
+const getStartedButton = document.getElementById("getStarted");
+const welcomeModal = document.querySelector(".welcome-modal");
+const closeModalButton = document.getElementById("closeMessage");
+
+// Functions for changing the CSS to display/hide the welcome modal
+function welcomeMessage() {
+    welcomeModal.style.display = "block";
+}
+
+function closeModal() {
+    welcomeModal.style.display = "none";
+}
+
+// Wait for user to click a button to open/close the welcome modal
+getStartedButton.addEventListener('click', welcomeMessage);
+closeModalButton.addEventListener('click', closeModal);

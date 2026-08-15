ccb0c4a926a3b2a2199129b8726c665bcfe1def6
omtriv23 <omtriv23@gmail.com>
2026-08-14 23:17:56 -0400

    V3 - Added the main modal opening code.
    After this addition to the JS, when the user clicks the button to close the welcome modal, not only will the welcome modal close, but it will also open up a new full-screen modal, where the bulk of the application will take place (such as uploading music files, creating study playlists, and setting study timers).

--- script.js
+++ script.js
@@ -4,6 +4,7 @@
 const getStartedButton = document.getElementById("getStarted");
 const welcomeModal = document.querySelector(".welcome-modal");
 const closeModalButton = document.getElementById("closeMessage");
+const mainModal = document.querySelector(".main-modal");
 
 // Functions for changing the CSS to display/hide the welcome modal
 function welcomeMessage() {
@@ -14,6 +15,15 @@ function closeModal() {
     welcomeModal.style.display = "none";
 }
 
+function displayMainModal() {
+    mainModal.style.display = "block";
+}
+
+function moveToMainModal() {
+    closeModal();
+    displayMainModal();
+}
+
 // Wait for user to click a button to open/close the welcome modal
 getStartedButton.addEventListener('click', welcomeMessage);
-closeModalButton.addEventListener('click', closeModal);
+closeModalButton.addEventListener('click', moveToMainModal);

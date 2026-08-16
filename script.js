6e1627ea00a5f40c7b72ce9501db25dac6aa7a27
omtriv23 <omtriv23@gmail.com>
2026-08-16 10:14:16 -0400

    V4 - Added open/close of new playlist form.
    The new additions to this JS code position the form and retrieve the form and "Create Playlist" button from the HTML code.

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const getStartedButton = document.getElementById("getStarted");
 const welcomeModal = document.querySelector(".welcome-modal");
 const closeModalButton = document.getElementById("closeMessage");
 const mainModal = document.querySelector(".main-modal");
+const createPlaylistButton = document.getElementById("createPlaylist");
+const playlistForm = document.getElementById("playlistForm");
 
 // Functions for changing the CSS to display/hide the welcome modal
 function welcomeMessage() {
@@ -24,6 +26,13 @@ function moveToMainModal() {
     displayMainModal();
 }
 
+function displayPlaylistForm() {
+    createPlaylistButton.style.display = "none";
+    playlistForm.style.display = "block";
+    playlistForm.style.marginTop = "2%"; // adding new CSS parameter and setting its value
+}
+
 // Wait for user to click a button to open/close the welcome modal
 getStartedButton.addEventListener('click', welcomeMessage);
 closeModalButton.addEventListener('click', moveToMainModal);
+createPlaylistButton.addEventListener('click', displayPlaylistForm);
